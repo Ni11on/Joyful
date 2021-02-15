4b7ae104c5e0ec9bf22157cda7cbf205490aaa3e
Nilton <niltonsjd@gmail.com>
2021-02-14 22:23:16 -0500

    Initial Commit

---
+++ Joyful/JoyfulApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoyfulApp.swift
+//  Joyful
+//
+//  Created by Ni11on on 14/02/21.
+//
+
+import SwiftUI
+
+@main
+struct JoyfulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
